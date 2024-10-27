82fc78fcd41d24505f713b83c6a06a2a3061bc53
nobuoooo <tsuchihashi.nobuyuki@ourly.co.jp>
2024-10-27 15:30:24 +0900

    add: アラビア数字を桁ごとにローマ数字に変化する処理をメソッド化した

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -27,4 +27,41 @@ class SgRomanizer
   def deromanize(roman)
     # write your code here
   end
+
+  private
+
+
+  # @param [Integer] arabic
+  # @param [Integer] digit
+  # @return [String]
+  def convert_arabic_to_roman(arabic, digit)
+    case digit
+    when 1, 2, 3
+      roman_string = ROMAN_STRING_MAP_BY_DIGITS.fetch(digit)
+      min_roman = roman_string[:min_roman]
+      middle_roman = roman_string[:middle_roman]
+      max_roman = roman_string[:max_roman]
+      case arabic
+      when 1, 2, 3
+        min_roman * arabic
+      when 4
+        min_roman + middle_roman
+      when 5, 6, 7, 8
+        middle_roman + (min_roman * (arabic - 5))
+      when 9
+        min_roman + max_roman
+      else
+        raise ArgumentError
+      end
+    when 4
+      case arabic
+      when 1, 2, 3
+        "M" * arabic
+      else
+        raise ArgumentError
+      end
+    else
+      raise ArgumentError
+    end
+  end
 end
