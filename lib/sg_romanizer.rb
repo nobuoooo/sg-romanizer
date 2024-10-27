cb53a0a259215840c302c33ab31b7ef475f23dd8
nobuoooo <tsuchihashi.nobuyuki@ourly.co.jp>
2024-10-27 21:48:21 +0900

    refactor: メソッド化、定数化

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -32,27 +32,12 @@ class SgRomanizer
   # @param [String] roman
   # @return [Integer]
   def deromanize(roman)
-    special_roman = roman.scan(/IV|IX|XL|XC|CD|CM/)
-    special_roman.each do |r|
-      roman.gsub!(r, '')
-    end
-    normal_roman = roman
-    special_roman_to_arabic_map =
-      {
-        "IV" => 4, "IX" => 9,
-        "XL" => 40, "XC" => 90,
-        "CD" => 400, "CM" => 900
-      }
-    result = special_roman.inject(0) do |acc, r|
-      acc + special_roman_to_arabic_map.fetch(r)
-    end
-    normal_roman_to_arabic_map =
-      {
-        "I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500, "M" => 1000
-      }
-    normal_roman.split('').inject(result) do |acc, r|
-      acc + normal_roman_to_arabic_map.fetch(r)
-    end
+    # 2文字でアラビア数字になるローマ数字のみ抽出
+    pair_roman_chars = roman.scan(PAIR_ROMAN_REGEX)
+    # 元のローマ数字から除外する
+    single_roman_chars = exclude_pairs_roman!(roman, pair_roman_chars).split('')
+    convert_single_roman_to_arabic(single_roman_chars) +
+      convert_pair_roman_to_arabic(pair_roman_chars)
   end
 
   private
@@ -89,6 +74,31 @@ class SgRomanizer
       end
     else
       raise ArgumentError
+    end
+  end
+
+  # @param [String] roman
+  # @param [Array] pair_roman_chars
+  # @return [String]
+  def exclude_pairs_roman!(roman, pair_roman_chars)
+    pair_roman_chars.each_with_object(roman) do |r, result|
+      result.gsub!(r, '')
+    end
+  end
+
+  # @param [Array] single_roman_chars
+  # @return [Integer]
+  def convert_single_roman_to_arabic(single_roman_chars)
+    single_roman_chars.inject(0) do |acc, char|
+      acc + SINGLE_ROMAN_TO_ARABIC_MAP.fetch(char)
+    end
+  end
+
+  # @param [Array] pair_roman_chars
+  # @return [Integer]
+  def convert_pair_roman_to_arabic(pair_roman_chars)
+    pair_roman_chars.inject(0) do |acc, char|
+      acc + PAIR_ROMAN_TO_ARABIC_MAP.fetch(char)
     end
   end
 end
