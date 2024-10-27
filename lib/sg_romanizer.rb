2d89d8c8251c696d1db2a1d8011b78d8d7ead5f0
nobuoooo <tsuchihashi.nobuyuki@ourly.co.jp>
2024-10-27 15:29:26 +0900

    add: ローマ数字を桁ごとにmap化した

--- lib/sg_romanizer.rb
+++ lib/sg_romanizer.rb
@@ -1,4 +1,25 @@
 class SgRomanizer
+  ROMAN_STRING_MAP_BY_DIGITS =
+    {
+      1 => {
+        min_roman: "I",
+        middle_roman: "V",
+        max_roman: "X"
+      },
+      2 => {
+        min_roman: "X",
+        middle_roman: "L",
+        max_roman: "C"
+      },
+      3 => {
+        min_roman: "C",
+        middle_roman: "D",
+        max_roman: "M"
+      }
+    }
+
+  # @param [Integer] arabic
+  # @return [String]
   def romanize(arabic)
     # write your code here
   end
